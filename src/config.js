8129bea2fab89b163bfa48065f173ae9592c0e32
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2023-04-24 19:47:19 -0300

    feat: setup config file to env variables

---
+++ src/config.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+const envPath = path.resolve(__dirname, '../.env');
+
+dotenv.config({
+  path: envPath,
+});
+
+module.exports = {
+  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  AWS_REGION: process.env.AWS_REGION,
+  AWS_API_VERSION: process.env.AWS_API_VERSION,
+  REDIS_HOST: process.env.REDIS_HOST,
+  REDIS_PORT: process.env.REDIS_PORT,
+};
