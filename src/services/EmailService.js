0af1084ba7cf4757ad5f7ca2430ad09bdea22452
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2023-04-24 19:50:05 -0300

    feat: add emailservice initial structure

---
+++ src/services/EmailService.js
@@ -0,0 +1,52 @@
+const AWS = require("aws-sdk");
+
+const {
+  AWS_ACCESS_KEY,
+  AWS_SECRET_ACCESS_KEY,
+  AWS_REGION,
+  AWS_API_VERSION,
+} = require("../config");
+
+class EmailService {
+  constructor() {
+    this.service = new AWS.SES({
+      region: AWS_REGION,
+      accessKeyId: AWS_ACCESS_KEY,
+      secretAccessKey: AWS_SECRET_ACCESS_KEY,
+      apiVersion: AWS_API_VERSION,
+    });
+  }
+
+  async send({ from, to, subject, text }) {
+    const params = {
+      Source: from,
+      Destination: {
+        ToAddresses: [to],
+      },
+      Message: {
+        Body: {
+          Text: {
+            Charset: "UTF-8",
+            Data: text,
+          },
+        },
+        Subject: {
+          Charset: "UTF-8",
+          Data: subject,
+        },
+      },
+    };
+
+    await this.service
+      .sendEmail(params)
+      .promise()
+      .then(() => {
+        console.log("E-mail enviado com sucesso!");
+      })
+      .catch(() => {
+        console.error("Falha no envio de e-mail!");
+      });
+  }
+}
+
+module.exports = new EmailService();
